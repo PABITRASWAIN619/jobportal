c60b8d90cc592ae3496469065911fb33c2242789
PABITRASWAIN619 <swainpabitra9861@gmail.com>
2026-05-02 10:25:43 +0530

    deploy

--- jobportal/jobportal/settings.py
+++ jobportal/jobportal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=k8hs%3r@i6(h*#b%2p$7y%^k(!x6kga5-nk$kgbtl0_#nw=i9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     'static',
