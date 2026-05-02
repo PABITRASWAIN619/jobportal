9db4134f5dc2ae16af935deeb888bb18ebc0b4ec
PABITRASWAIN619 <swainpabitra9861@gmail.com>
2026-05-02 18:05:10 +0530

    add setting.py again

--- jobportal/jobportal/settings.py
+++ jobportal/jobportal/settings.py
@@ -157,3 +157,7 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
+CSRF_TRUSTED_ORIGINS = [
+    "https://barsapalei.onrender.com",
+    "https://easypalei.onrender.com"
+]
