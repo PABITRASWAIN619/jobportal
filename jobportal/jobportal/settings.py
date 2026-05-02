09c261175ec87908104982a939ef5d8c98e4c846
PABITRASWAIN619 <swainpabitra9861@gmail.com>
2026-05-02 18:00:01 +0530

    add render

--- jobportal/jobportal/settings.py
+++ jobportal/jobportal/settings.py
@@ -11,7 +11,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key')
 
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['easypalei.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = [
+    'barsapalei.onrender.com',
+    'easypalei.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # =========================
